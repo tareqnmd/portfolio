62094a110b8cb04b1ff8c8937b6e5eef742ec6d4
Tareq <md.tareq@asthait.com>
2025-09-10 16:19:25 +0600

    Enhance Astro project setup with React, Vue, and Tailwind CSS integration. Update configuration and dependencies, replace SVG assets with PNG, and refactor Welcome component for a cleaner layout. Remove unused SVG files and update favicon to ICO format.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
-// @ts-check
+import react from '@astrojs/react';
+import vue from '@astrojs/vue';
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	vite: {
+		plugins: [tailwindcss()],
+	},
+	integrations: [react(), vue()],
+});
